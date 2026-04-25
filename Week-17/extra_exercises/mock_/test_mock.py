5fb901546e34e2691c63aa110597bd513523c39b
Shalston <shalston97@gmail.com>
2026-04-25 15:04:31 -0600

    Week-17: functions exercises and mock tests

---
+++ Week-17/extra_exercises/mock_/test_mock.py
@@ -0,0 +1,26 @@
+from unittest.mock import mock_open, patch
+import pytest
+from mock_exercise import read_file_lines
+
+def test_read_file_returns_expected_lines():
+    #Arrange
+    fake_content = "hola\nmundo\npython"
+    expected = ["hola", "mundo", "python"]
+
+    #Act
+    with patch("builtins.open", mock_open(read_data=fake_content)):
+        result = read_file_lines("fake_path.txt")
+
+    #Assert
+    assert result == expected
+
+
+def test_read_file_raises_error_if_not_found():
+    #Arrange
+    mock = mock_open()
+    mock.side_effect = FileNotFoundError
+
+    #Act & Assert
+    with patch("builtins.open", mock):
+        with pytest.raises(FileNotFoundError):
+            read_file_lines("archivo_inexistente.txt")
