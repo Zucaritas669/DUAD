89a0294a9bc04f82d30a80c65407d46a5d67febc
Shalston <shalston97@gmail.com>
2026-06-20 22:26:53 -0600

    Rpository Pattern

---
+++ Week-26-repository_pattern/app.py
@@ -0,0 +1,218 @@
+from flask import Flask, request , jsonify
+from db import PgManager
+from repository import UserRepository , CarRepository, RentalRepository
+
+# cd Week-26-repository_pattern
+# python app.py
+
+app = Flask(__name__)
+#code
+
+# Creamos (instanciamos) el PgManager con los datos de conexión a la base.
+# Luego se lo pasamos al Repository para que pueda hacer sus queries.
+db_manager = PgManager(
+    db_name = "postgres",
+    user = "postgres",
+    password = "postgres",
+    host = "localhost"
+)
+user_repo = UserRepository(db_manager)
+car_repo = CarRepository(db_manager)
+rental_repo = RentalRepository(db_manager)
+#==========================================  CREATE Routes ================================
+
+#========== Create USER ==============
+@app.route("/users", methods=["POST"])
+def create_user():
+    try:
+        
+        if "name" not in request.json:
+            raise ValueError("name missing")
+        
+        if "username" not in request.json:
+            raise ValueError("username missing")
+        
+        if "email" not in request.json:
+            raise ValueError("email missing")
+        
+        if "password" not in request.json:
+            raise ValueError("password missing")
+        
+        if "birth_date" not in request.json:
+            raise ValueError("birth_date missing")
+        
+        user_repo.create_user(
+            request.json["name"],
+            request.json["username"],
+            request.json["email"],
+            request.json["password"],
+            request.json["birth_date"]
+        )
+        return {"Status":"User created"},200
+    except ValueError as ex:
+        return jsonify(message = str(ex)),400
+
+#========== Create CAR ===============
+@app.route("/cars", methods = ["POST"])
+def create_car():
+    try:
+        if "brand" not in request.json:
+            raise ValueError("Brand missing")
+        
+        if "model" not in request.json:
+            raise ValueError("Model missing")  
+        
+        if "manufacture_year" not in request.json:
+            raise ValueError("manufacture_year missing")  
+        
+        car_repo.create_car(
+            request.json["brand"],
+            request.json["model"],
+            request.json["manufacture_year"]
+        )
+
+        return {"Status":"Car created"},200
+    except ValueError as ex:
+        return jsonify(message = str(ex)),400
+
+
+#========== Create rental ===============
+@app.route("/rentals", methods = ["POST"])
+def create_rental():
+    try:
+        if "user_id" not in request.json:
+            raise ValueError("User_id missing")
+        
+        if "car_id" not in request.json:
+            raise ValueError("Car_id missing")
+        
+        rental_repo.create_rental(
+            request.json["user_id"],
+            request.json["car_id"],
+        )
+        return{"Status":"Rental created"},200
+    except ValueError as ex:
+        return jsonify(message = str(ex)),400
+    
+
+#==========================================  MODIFY Routes ================================
+
+#========== Change car status =====================
+valid_status = ["available", "rented", "disabled"]
+
+@app.route("/cars/<int:car_id>", methods = ["PATCH"])
+def change_car_status(car_id):
+    try:
+        if "status" not in request.json:
+            raise ValueError("Status missing")
+        
+        if request.json["status"] not in valid_status:
+            raise ValueError("Invalid status")
+        
+
+        car_repo.change_status(
+            request.json["status"],car_id) # car_id es para verificar cual auto es
+
+        return {"Status": "Car status updated"}, 200
+    except ValueError as ex:
+        return jsonify(message=str(ex)), 400
+    
+
+    
+#========== Change user status =====================
+valid_account_status = ["active", "disabled", "morose"]
+
+@app.route("/users/<int:user_id>",methods = ["PATCH"])
+def change_user_account_status(user_id):
+    try:
+        if "account_status" not in request.json:
+            raise ValueError("Account status missing")
+        
+        if request.json["account_status"] not in valid_account_status:
+            raise ValueError("Invalid account status")
+        
+        user_repo.change_user_status(request.json["account_status"],user_id)
+
+        return{"Status":"User account status updated"},200
+    except ValueError as ex:
+        return jsonify(message = f"Error with the endpoint {ex}"),400
+
+
+
+#========== Change rental status =====================
+
+@app.route("/rentals/<int:rental_id>/completed", methods = ["PATCH"])
+def rental_status_completed(rental_id):
+
+    rental_repo.complete_rental(rental_id)
+    return {"Status":"Rental completed"},200
+
+#Por que se hace así?
+#Se hace así porque queremos marcar el status como "completed" sin mas,
+#no hay mas opciones, entonces no se cpmprueba nada ni request , ya que es un estado único
+
+#Adicionalmente se pasa el valor al app.rout de ina vez sin tanta complicaión
+
+
+#========== Change rental status 2 =====================
+rental_status = ["active","canceled"]
+
+@app.route("/rentals/<int:rental_id>", methods = ["PATCH"])
+def change_status_rental_two(rental_id):
+    try:
+        if "rental_status" not in request.json:
+            raise ValueError("Rental Status missing")
+        
+        if request.json["rental_status"] not in rental_status:
+            raise ValueError("Invalid rental Status")
+        
+
+        rental_repo.change_status_rental(request.json["rental_status"],rental_id)
+        return{"Status":"Rental Status changed"},200
+    except ValueError as ex:
+        return jsonify(message = f"Error with the endpoint {ex}"),400
+
+
+#========== Change user status morose =====================
+@app.route("/users/<int:user_id>/morose", methods = ["PATCH"])
+def morose_status(user_id):
+        
+        user_repo.morose_flag(user_id)
+        return {"Status":"Morose status updated"},200
+
+
+
+#==========================================  filter Routes ================================
+
+@app.route("/users", methods=["GET"])
+def list_users():
+    filters = request.args.to_dict()    # agarra todos los query params como dict
+    users = user_repo.get_all(filters)  # se los pasa al repo
+    return {"users": users}, 200
+
+
+@app.route("/cars", methods = ["GET"])
+def get_all_cars():
+    result = request.args.to_dict()
+    cars = car_repo.get_all_cars(result)
+    return {"cars":cars},200
+
+
+@app.route("/rentals",methods = ["GET"])
+def get_all_rentals():
+    result = request.args.to_dict()
+    rental = rental_repo.get_rentals(result)
+    return {"rentals":rental}
+
+
+
+
+#code
+if __name__ == "__main__":
+    app.run(host ="localhost", debug=True)
+
+
+
+
+
+
